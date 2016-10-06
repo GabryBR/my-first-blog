ddd2a4580485f835bf0b0bbe0a61b6a87043d492
GabryBR <gabriele.brunero@hotmail.com>
2016-10-06 12:01:48 +0200

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -5,7 +5,7 @@ path = "/home/gabrybr/my-first-blog/mysite"
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite'
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
